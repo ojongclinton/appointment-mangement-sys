22958541ef63164535d2e1bf47c1623c4ee245a1
william enoh <sheyenoh123@gmail.com>
2022-02-24 21:05:04 +0100

    fewer

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ mongoose.connection.on('connected',()=>{
 })
 
 let app = express();
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("Listening on port 5000")
 })
 app.get('/',(req,res)=>{
